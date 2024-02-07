b32fe328c988f3431980d50be325a9f9ebf85446
Averjahn <ogoka2001@gmail.com>
2024-02-08 01:40:41 +0300

    Add files via upload

---
+++ assets/ContentSubtitle-25fa415c.js
@@ -0,0 +1,1 @@
+import{u as s,o,c}from"./index-a8985bbb.js";const l=["innerHTML"],_={__name:"ContentSubtitle",props:["componentData","testStyle"],setup(t){const e=t;return s(n=>({"3414d189":t.testStyle.subtitleColor})),(n,a)=>(o(),c("div",{innerHTML:e.componentData.subtitle,class:"content-subtitle"},null,8,l))}};export{_ as default};
