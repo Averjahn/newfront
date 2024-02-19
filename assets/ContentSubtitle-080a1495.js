fc9370dd68b23b46db4e00e0add3e82e0cfe2c07
Averjahn <ogoka2001@gmail.com>
2024-02-19 11:50:19 +0300

    Delete assets directory

--- assets/ContentSubtitle-080a1495.js
+++
@@ -1,1 +0,0 @@
-import{u as s,o,c}from"./index-439f8bb7.js";const l=["innerHTML"],_={__name:"ContentSubtitle",props:["componentData","testStyle"],setup(t){const e=t;return s(n=>({"3414d189":t.testStyle.subtitleColor})),(n,a)=>(o(),c("div",{innerHTML:e.componentData.subtitle,class:"content-subtitle"},null,8,l))}};export{_ as default};
